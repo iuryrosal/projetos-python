f6e1e2aacfbf34c682905012a4d48eac8f74082d
Iury Lima Rosal <iuryrosal@gmail.com>
2022-03-26 19:20:48 -0300

    ✨ Adiciona projeto de detecção de palindromos usando recursividade

---
+++ level-a/detect_palindromes.py
@@ -0,0 +1,28 @@
+words = ["arara", "racecar", "carro", "level"]
+
+def is_palindrome(word):
+    j = len(word)-1
+    result = 0
+    for i in range(0, len(word)):
+        if word[i] == word[j]:
+            result = result + 1
+        if i >= j:
+            break
+
+        j = j - 1
+
+    if result == i+1:
+        return True
+    else:
+        return False
+
+
+def is_palindrome_recursive(word):
+    if len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and is_palindrome_recursive(word[1:-1])
+
+for word in words:
+    print(word)
+    print(is_palindrome(word))
