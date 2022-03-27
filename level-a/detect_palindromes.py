39f87cae0780181cc2cd074c308edac8aef146bb
Iury Lima Rosal <iuryrosal@gmail.com>
2022-03-27 14:58:31 -0300

    ♻️ Atualiza código de detecção de palindromos

--- level-a/detect_palindromes.py
+++ level-a/detect_palindromes.py
@@ -1,21 +1,19 @@
-words = ["arara", "racecar", "carro", "level"]
+import math
 
 def is_palindrome(word):
     j = len(word)-1
     result = 0
-    for i in range(0, len(word)):
+    for i in range(len(word)):
         if word[i] == word[j]:
             result = result + 1
         if i >= j:
             break
-
         j = j - 1
-
-    if result == i+1:
+    
+    if result == math.ceil(len(word)/2):
         return True
     else:
         return False
-
 
 def is_palindrome_recursive(word):
     if len(word) <= 1:
@@ -23,6 +21,10 @@ def is_palindrome_recursive(word):
     else:
         return word[0] == word[-1] and is_palindrome_recursive(word[1:-1])
 
+
+words = ["arara", "racecar", "carro", "cama", "level"]
+
 for word in words:
     print(word)
-    print(is_palindrome(word))
+    print(is_palindrome_recursive(word))
+    print("\n")
